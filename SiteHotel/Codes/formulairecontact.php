50e1b83f529ec95b639f1729e56c47a6c15ba2c2
AlexandreSIO1 <alexandre.pryimak@stcharles-stecroix.org>
2026-03-01 19:52:44 +0100

    Add files via upload

--- SiteHotel/Codes/formulairecontact.php
+++ SiteHotel/Codes/formulairecontact.php
@@ -2,5 +2,10 @@
 $nom = $_POST["nom"];
 $prenom = $_POST["prenom"];
 $email = $_POST["email"];
-echo "Nom: ", $nom, "<br>", "Prénom: ", $prenom, "<br>", "Email: ", $email;
+$message = $_POST["message"];
+
+echo "Nom : ", $nom, "<br>";
+echo "Prénom : ", $prenom, "<br>";
+echo "Email : ", $email, "<br><br>";
+echo "Message reçu : <br>", $message;
 ?>
